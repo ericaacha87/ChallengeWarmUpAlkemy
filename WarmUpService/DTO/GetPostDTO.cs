ec0f23db912ea2262f6d15cd11fdf599c751c3c5
EricaAcha <erica.acha@gmail.com>
2021-07-22 19:45:24 -0300

    bug repaired

---
+++ WarmUpService/DTO/GetPostDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarmUpService.DTO
+{
+    public class GetPostDTO
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+      
+    }
+}
