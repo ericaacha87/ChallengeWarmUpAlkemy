e64428cdee12259531a1bb439709d95fad32883b
EricaAcha <erica.acha@gmail.com>
2021-07-22 15:29:01 -0300

    frontEnd was modified, controller added

---
+++ FrontEnd/Controllers/PostController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using FrontEnd.Models;
+namespace FrontEnd.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            // call api and get all posts
+            IEnumerable<GetPosts> posts = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44307/api");
+                var responseTask = client.GetAsync("GetPosts");
+                responseTask.Wait();
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<GetPosts>>();
+                    readTask.Wait();
+
+                    posts = readTask.Result;
+                }
+                else
+                {
+                    posts = Enumerable.Empty<GetPosts>();
+                    ModelState.AddModelError(string.Empty, "Try Later.");
+                }
+            }
+                return View("Index",posts);
+        }
+
+        // GET: Post/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Post/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Post/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FrontEnd/Models/GetPosts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrontEnd.Models
+{
+    [Serializable]
+    public class GetPosts
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Imagen { get; set; }
+        public string Categoria { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
